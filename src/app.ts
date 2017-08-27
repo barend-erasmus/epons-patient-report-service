cb8388c553d0fa5a221758c458c2fd185ed6e719
Barend Erasmus <developersworkspace@gmail.com>
2017-08-27 20:30:51 +0200

    update

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,14 @@ import * as express from 'express';
 import * as path from 'path';
 import * as yargs from 'yargs';
 import * as cors from 'cors';
+import * as expressWinston from 'express-winston';
 
 // Imports middleware
 import * as bodyParser from 'body-parser';
 import * as exphbs from 'express-handlebars';
+
+// Imports logger
+import { logger } from './logger';
 
 // Imports routes
 import { HomeRouter } from './routes/home';
@@ -19,6 +23,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({limit: '50mb'}));
 
 app.use(cors());
+
+app.use(expressWinston.logger({
+    meta: true,
+    msg: 'HTTP Request: {{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}} {{req.ip}}',
+    winstonInstance: logger,
+}));
+
 
 // Configures static content
 app.use('/static', express.static(path.join(__dirname, 'public')));
---
+++ src/logger.ts
@@ -0,0 +1,19 @@
+// Imports
+import * as path from 'path';
+import * as winston from 'winston';
+import * as yargs from 'yargs';
+
+const argv = yargs.argv;
+
+const logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)({ level: 'debug' }),
+    new (winston.transports.File)({
+      filename: path.join(__dirname, 'epons-patient-report-service.log'),
+      level: 'debug',
+    }),
+  ],
+});
+
+// Exports
+export { logger };
